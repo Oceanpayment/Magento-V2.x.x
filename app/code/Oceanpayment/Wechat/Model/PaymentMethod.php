2937abb0769258bfb62c0a66aab6d938e387fe9d
Oceanpayment <102337499+Oceanpayment-Dev@users.noreply.github.com>
2022-04-13 15:40:12 +0800

    update wechat payment method

--- app/code/Oceanpayment/Wechat/Model/PaymentMethod.php
+++ app/code/Oceanpayment/Wechat/Model/PaymentMethod.php
@@ -131,7 +131,7 @@ class PaymentMethod extends AbstractMethod
         //securecode
         $securecode        = $this->getConfigData('securecode');
         //支付方式
-        $methods           = 'WX';
+        $methods           = $this->Source();
         //订单号
         $order_number      = $orderIncrementId;  
         //返回地址
@@ -271,30 +271,57 @@ class PaymentMethod extends AbstractMethod
 
 
     /**
-     * 判断是否手机设备
+     * 检验是否移动端
      */
-    public function isMobile(){
-        $useragent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
-        $useragent_commentsblock = preg_match('|\(.*?\)|', $useragent, $matches) > 0 ? $matches[0] : '';
-
-        function CheckSubstrs($substrs, $text){
-            foreach($substrs as $substr){
-                if(false !== strpos($text, $substr)){
-                    return true;
-                }
-            }
-            return false;
+    function isMobile(){
+        // 如果有HTTP_X_WAP_PROFILE则一定是移动设备
+        if (isset ($_SERVER['HTTP_X_WAP_PROFILE'])){
+            return true;
         }
-
-        $mobile_os_list = array('Google Wireless Transcoder','Windows CE','WindowsCE','Symbian','Android','armv6l','armv5','Mobile','CentOS','mowser','AvantGo','Opera Mobi','J2ME/MIDP','Smartphone','Go.Web','Palm','iPAQ');
-        $mobile_token_list = array('Profile/MIDP','Configuration/CLDC-','160×160','176×220','240×240','240×320','320×240','UP.Browser','UP.Link','SymbianOS','PalmOS','PocketPC','SonyEricsson','Nokia','BlackBerry','Vodafone','BenQ','Novarra-Vision','Iris','NetFront','HTC_','Xda_','SAMSUNG-SGH','Wapaka','DoCoMo','iPhone','iPod');
-
-        $found_mobile = CheckSubstrs($mobile_os_list, $useragent_commentsblock) || CheckSubstrs($mobile_token_list,$useragent);
-
-        if ($found_mobile){
-            return 1;   //手机登录
+        // 如果via信息含有wap则一定是移动设备,部分服务商会屏蔽该信息
+        if (isset ($_SERVER['HTTP_VIA'])){
+            // 找不到为flase,否则为true
+            return stristr($_SERVER['HTTP_VIA'], "wap") ? true : false;
+        }
+        // 判断手机发送的客户端标志
+        if (isset ($_SERVER['HTTP_USER_AGENT'])){
+            $clientkeywords = array (
+                'nokia','sony','ericsson','mot','samsung','htc','sgh','lg','sharp','sie-','philips','panasonic','alcatel',
+                'lenovo','iphone','ipod','blackberry','meizu','android','netfront','symbian','ucweb','windowsce','palm',
+                'operamini','operamobi','openwave','nexusone','cldc','midp','wap','mobile'
+            );
+            // 从HTTP_USER_AGENT中查找手机浏览器的关键字
+            if (preg_match("/(" . implode('|', $clientkeywords) . ")/i", strtolower($_SERVER['HTTP_USER_AGENT']))){
+                return true;
+            }
+        }
+        // 判断协议
+        if (isset ($_SERVER['HTTP_ACCEPT'])){
+            // 如果只支持wml并且不支持html那一定是移动设备
+            // 如果支持wml和html但是wml在html之前则是移动设备
+            if ((strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') !== false) && (strpos($_SERVER['HTTP_ACCEPT'], 'text/html') === false || (strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') < strpos($_SERVER['HTTP_ACCEPT'], 'text/html')))){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * 判断终端来源
+     */
+    function Source(){
+        //是否移动端
+        if($this->isMobile()){
+            if(strpos($_SERVER['HTTP_USER_AGENT'],'MicroMessenger') !== false){            
+                //公众号
+                return 'WeChatPay_Acc';            
+            }else{
+                //H5
+                return 'WeChatPay_Wap';
+            }    
         }else{
-            return 0;  //电脑登录
+            //pc
+            return 'WeChatPay_Web';
         }
     }
 
