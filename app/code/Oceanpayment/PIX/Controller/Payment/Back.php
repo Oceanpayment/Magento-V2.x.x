8c662709ade7a068b509a6689cdfc55203b75ab6
Oceanpayment <102337499+Oceanpayment@users.noreply.github.com>
2023-04-28 17:52:39 +0800

    bug fix

--- app/code/Oceanpayment/PIX/Controller/Payment/Back.php
+++ app/code/Oceanpayment/PIX/Controller/Payment/Back.php
@@ -111,6 +111,7 @@ class Back extends \Magento\Framework\App\Action\Action implements CsrfAwareActi
 
                 $order->save();
 
+		$this->checkoutSession->getQuote();
                 $url = 'checkout/onepage/success';
                 break;
             case 0:
