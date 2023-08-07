89070a912ba128425dd3932d2fe8cf6cc284074c
刘强 <stevenliu@oceanpayment.com.cn>
2023-08-07 18:20:48 +0800

    优化二次推送

--- app/code/Oceanpayment/Alipay/Controller/Payment/Back.php
+++ app/code/Oceanpayment/Alipay/Controller/Payment/Back.php
@@ -221,24 +221,30 @@ class Back extends \Magento\Framework\App\Action\Action implements CsrfAwareActi
         
         //加密校验
         if(strtoupper($local_signValue) == strtoupper($back_signValue)){
-            //在网站中已经是支付成功
-            if(in_array($order->getState(), $this->_processingArray)){
-                return 2;
-            }
 
             //支付状态
             if ($payment_status == 1) {
                 return 1;
             } elseif ($payment_status == -1) {
-                return -1;
-            } elseif ($payment_status == 0) {
-
-                //是否点击浏览器后退造成订单号重复 20061
-                if($getErrorCode[0] == '20061'){
-                    return '20061';
+                //在网站中已经是支付成功
+                if(in_array($order->getState(), $this->_processingArray)){
+                    return 2;
+                }else{
+                    return -1;
                 }
+            } elseif ($payment_status == 0) {
+                //在网站中已经是支付成功
+                if(in_array($order->getState(), $this->_processingArray)){
+                    return 2;
+                }else{
+                    //是否点击浏览器后退造成订单号重复 20061
+                    if($getErrorCode[0] == '20061'){
+                        return '20061';
+                    }
 
-                return 0;
+                    return 0;
+                }
+                
             }
         }else{
             return 999;
