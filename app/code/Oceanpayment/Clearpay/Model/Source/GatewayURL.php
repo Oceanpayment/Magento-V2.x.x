d4cdd3b45a55415ef9e7a428469a7aed27a1a1b5
susanwan <Susanwan@oceanpayment.com.cn>
2023-08-31 11:24:03 +0800

    Clearpay

---
+++ app/code/Oceanpayment/Clearpay/Model/Source/GatewayURL.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Oceanpayment\Clearpay\Model\Source;
+
+use Magento\Framework\Option\ArrayInterface;
+
+class GatewayURL implements ArrayInterface {
+
+    /**
+     * @return array
+     */
+    public function toOptionArray() {
+        return [
+            ['value' => 'https://secure.oceanpayment.com/gateway/service/pay', 'label' => __('Production')],
+            ['value' => 'https://test-secure.oceanpayment.com/gateway/service/pay', 'label' =>__('Sandbox')]
+        ];
+    }
+}
+
